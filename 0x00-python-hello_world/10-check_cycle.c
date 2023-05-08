86dd6e4e6eaf7970e9be61441998f75e8aa3965e
Stephanie <Stephaniechloe21@gmail.com>
2023-05-08 13:37:57 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,12 +4,14 @@ int check_cycle(listint_t *list)
 {
 	listint_t *ptr;
 	listint_t **nodes;
-
 	int i;
+
+	if (list == NULL)
+		return (0);
 
 	nodes = malloc(sizeof(listint_t) * 1000);
 	ptr = list;
-
+	ptr2 = list->next;
 	while (ptr != NULL)
 	{
 		for (i = 0; nodes[i] != NULL; i++)
