6158f1f242e4b6451f1a6c67860ccad9b6cce10a
Stephanie <Stephaniechloe21@gmail.com>
2023-05-10 18:19:01 -0700

    task 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add(a, b):
-    is_digit= str(a).isdigit() if a > 0 else str(-a).isdigit()
-    is_digit= str(b).isdigit() if b > 0 else str(-b).isdigit()
+    is_digit = str(a).isdigit() if a > 0 else str(-a).isdigit()
+    is_digit = str(b).isdigit() if b > 0 else str(-b).isdigit()
     sum = ''
     if is_digit:
         sum = a + b
