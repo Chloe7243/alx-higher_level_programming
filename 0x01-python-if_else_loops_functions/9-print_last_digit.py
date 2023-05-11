fe65ec1a3951e71a5813db9fc48e71d1e1698aeb
Stephanie <Stephaniechloe21@gmail.com>
2023-05-10 17:58:05 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number.isdigit():
+    if str(number).isdigit():
         last_digit = str(number)[-1]
         print("{}".format(last_digit), end='')
         return last_digit
