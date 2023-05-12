e6962f750a07b767ce629edbc0ccd00389dca642
Stephanie <Stephaniechloe21@gmail.com>
2023-05-11 22:44:23 -0700

    pyccodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
         exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    operation = {'+':add, '-':sub, '*':mul, '/':div}
+    operation = {'+': add, '-': sub, '*': mul, '/': div}
     for op in operation:
         if argv[2] == op:
             print(f"{a} {op} {b} = {operation[op](a, b)}")
-            exit (0)
+            exit(0)
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
