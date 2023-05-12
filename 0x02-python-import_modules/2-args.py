22cc7eb0ebae3b892d4d3764ac5de0c52c35883d
Stephanie <Stephaniechloe21@gmail.com>
2023-05-11 18:45:32 -0700

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    len_args = len(sys.argv) - 1
+    from sys import argv
+    len_args = len(argv) - 1
     if len_args > 0:
         if len_args == 1:
             print("1 argument:")
         else:
             print(f"{len_args} arguments:")
-            for i, arg in enumerate(sys.argv):
+            for i, arg in enumerate(argv):
                 if i == 0:
                     continue
                 print(f"{i}: {arg}")
