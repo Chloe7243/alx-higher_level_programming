91deae0effcbca5a0b36aa952dd738dbdda4e74c
Stephanie <Stephaniechloe21@gmail.com>
2023-05-13 07:58:56 -0700

    task 6 fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for i, y in enumerate(x):
             print("{:d}".format(y), end=' ' if i < len(x) - 1 else '\n')
-if len(matrix[0]) == 0:
-    print("")
+    if len(matrix[0]) == 0:
+        print("")
