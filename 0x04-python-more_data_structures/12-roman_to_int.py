837ad473a9f2e957bda65356b6ca4a1d7720a6af
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 11:42:08 -0700

    i think i got it right this time

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,16 +6,37 @@ def roman_to_int(roman_string):
         {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     roman_string = roman_string.upper()
-    if len(roman_string) > 1 and \
-            main_symbols[roman_string[0]] < main_symbols[roman_string[1]]:
-        value = main_symbols[roman_string[-1]]
-        for i, x in enumerate(reversed(roman_string)):
-            if i != 0:
-                value -= main_symbols[x]
-        return value
 
-    for x in roman_string:
+    for i, x in enumerate(roman_string):
         if x not in main_symbols:
             return 0
+        if len(roman_string) > 1 and \
+                main_symbols[x]  < main_symbols[roman_string[-1]]:
+            value += (main_symbols[roman_string[-1]] - main_symbols[x])
+            return value
         value += main_symbols[x]
     return value
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "l"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "liv"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
