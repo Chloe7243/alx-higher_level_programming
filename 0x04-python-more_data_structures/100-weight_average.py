002bea9838e0119484a0dc0bd3fba1f995a92f49
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 12:38:44 -0700

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     numerator, denominator = 0, 0
     for x in my_list:
-        num1 , num2 = x
+        num1, num2 = x
         numerator += num1 * num2
         denominator += num2
     return numerator / denominator
