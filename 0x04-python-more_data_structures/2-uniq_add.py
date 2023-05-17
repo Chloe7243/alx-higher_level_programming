4e50d488f82736afd10c5bb1b0c298b60f936851
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 07:44:14 -0700

    task 2 fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -7,7 +7,3 @@ def uniq_add(my_list=[]):
             list_sum += x
             num = x
     return list_sum
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
