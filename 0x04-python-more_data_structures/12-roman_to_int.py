b2ec62727ce4bd8d118a3517bd8b3643a11be358
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 12:00:00 -0700

    forgot to remove test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     main_symbols = \
         {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
+    roman_string = roman_string.upper()
 
     for i, x in enumerate(roman_string):
         if x not in main_symbols:
