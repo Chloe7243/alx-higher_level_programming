a605b76048b66e383de1bd3cbb2f895a7754a116
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 07:49:36 -0700

    final fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,8 @@
 def uniq_add(my_list=[]):
     my_list.sort()
     num, list_sum = my_list[0], 0
-    for x in my_list:
-        if x > num:
+    for i, x in enumerate(my_list):
+        if x > num or i == 0:
             list_sum += x
             num = x
     return list_sum
