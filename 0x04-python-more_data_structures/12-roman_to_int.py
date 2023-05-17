5c5166bf41c7754f1ab892bf45fa947d2a74f535
Stephanie <Stephaniechloe21@gmail.com>
2023-05-17 12:10:47 -0700

    task 12 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,11 +10,8 @@ def roman_to_int(roman_string):
     for i, x in enumerate(roman_string):
         if x not in main_symbols:
             return 0
-        if roman_string.count(x) > 3:
-            return 0
         if len(roman_string) > 1 and \
                 main_symbols[x] < main_symbols[roman_string[-1]]:
             value += (main_symbols[roman_string[-1]] - main_symbols[x])
-            return value
         value += main_symbols[x]
     return value
