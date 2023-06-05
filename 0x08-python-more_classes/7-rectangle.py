2a43f2ecbbaf54f0f815db270b9a6a51a9e3e614
Stephanie <Stephaniechloe21@gmail.com>
2023-06-05 12:42:23 -0700

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -48,7 +48,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         else:
-            return '\n'.join([str(self.print_symbol) * self.width] * self.height)
+            return '\n'.join([str(self.print_symbol) * self.width]\
+                    * self.height)
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
