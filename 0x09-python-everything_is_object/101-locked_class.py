9b7511a0052d43be69f82a1bba897f2069610102
Stephanie <Stephaniechloe21@gmail.com>
2023-06-06 05:47:32 -0700

    task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a class: LockedClass"""
 
-
 class LockedClass:
+    """A class that only allows first_name attribute"""
+
     __slots__ = ["first_name"]
     def __setattr__(self, name, value):
         object.__setattr__(self, name, value)
