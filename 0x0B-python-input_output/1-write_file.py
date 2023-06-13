e5a4302dbc91ad1bb0745e1be1ee3cf92c5dfa14
Stephanie <Stephaniechloe21@gmail.com>
2023-06-13 16:59:42 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a function that writes a string to a text\
+        file (UTF8) and returns the number of characters written """
+
+
+def read_file(filename="", text=""):
+    with open(filename, w) as f:
+        characters = f.write(text)
+        f.close()
+        return characters
