f4ee09f2c2fd6822dc9996561401f97940889df3
Stephanie <Stephaniechloe21@gmail.com>
2023-06-13 17:25:17 -0700

    task 1 - 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,3 @@
 def write_file(filename="", text=""):
     with open(filename, "w") as f:
         return f.write(text)
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
