dc0eae06c0b129c4cadb4d3b853d189400d9ee89
Stephanie <Stephaniechloe21@gmail.com>
2023-06-13 17:02:32 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,11 @@
         file (UTF8) and returns the number of characters written """
 
 
-def read_file(filename="", text=""):
-    with open(filename, w) as f:
+def write_file(filename="", text=""):
+    with open(filename, 'w') as f:
         characters = f.write(text)
         f.close()
         return characters
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
