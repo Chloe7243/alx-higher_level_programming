8ebc068e9f79e041c7892e3e8f29a495672e8f16
Stephanie <Stephaniechloe21@gmail.com>
2023-06-13 17:33:21 -0700

    hmm

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,5 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, "w", encoding="utf-8") as fl:
+        return fl.write(text)
