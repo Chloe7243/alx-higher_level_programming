7d104aeb133f0fdf7b2a99b645e28cccacc3c167
Stephanie <Stephaniechloe21@gmail.com>
2023-06-13 17:33:54 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,10 @@
 
 
 def write_file(filename="", text=""):
+    """
+        function receives a filename and a text
+        then writes the text into the file with the file name
+        returning the number of characters written
+    """
     with open(filename, "w", encoding="utf-8") as fl:
         return fl.write(text)
