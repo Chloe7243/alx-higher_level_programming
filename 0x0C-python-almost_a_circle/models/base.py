08af2ee14bbf2e16f288c6d523f2375d6e8fdf89
Stephanie <Stephaniechloe21@gmail.com>
2023-06-19 12:25:18 -0700

    figuring shit out

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A class called Base """
+
+
+class Base:
+    """Base Class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = id
