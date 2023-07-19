0617b133781b33c93924086287dc91f496b38867
Stephanie <Stephaniechloe21@gmail.com>
2023-07-18 18:32:47 -0700

    finally

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 --  converts all of the following to UTF8: Database hbtn_0c_0, Table first_table, Field name in first_table
-ALTER hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
