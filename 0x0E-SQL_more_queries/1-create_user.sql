6374914c923fe0f50b6d469d8dc8d84be4aacd06
Stephanie <Stephaniechloe21@gmail.com>
2023-07-19 21:46:44 -0700

    tired

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- creates the MySQL server user user_0d_1.
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT, INSERT, ... , DROP ROLE ON *.* TO `user_0d_1`@`localhost`;
-GRANT APPLICATION_PASSWORD_ADMIN, ... , XA_RECOVER_ADMIN ON *.* TO `user_0d_1`@`localhost`;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
