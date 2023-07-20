0c8ccb7a0280b72aef3f6f42a1296a546dd16ad5
Stephanie <Stephaniechloe21@gmail.com>
2023-07-19 21:20:03 -0700

    task 18 fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,3 +1,3 @@
 -- uses the hbtn_0d_tvshows database to lists all shows of the comedy genre.
 
-SELECT title from tv_shows WHERE title NOT IN (SELECT tv_shows.title FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_genres.name = 'Comedy') ORDER BY tv_shows.title ASC
+SELECT tv_shows.title from tv_shows WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_genres.name = 'Comedy') ORDER BY tv_shows.title ASC
