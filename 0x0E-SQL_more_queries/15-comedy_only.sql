54d697430b54c622988f4010aa31e1d8ddce43e7
Stephanie <Stephaniechloe21@gmail.com>
2023-07-19 19:29:36 -0700

    replaced 14

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+--  uses the hbtn_0d_tvshows database to lists all shows of the comedy genre.
+SELECT tv_shows.title FROM tv_genres, tv_shows, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC
