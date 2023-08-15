d62db00cf422b820a463242abca1ca365038edf2
Stephanie <Stephaniechloe21@gmail.com>
2023-08-15 15:47:21 +0000

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const MSquare = require('./5-square');
 module.exports = class Square extends MSquare {
-  constructor(size) {
+  constructor (size) {
     super(size);
+    this.size = size;
   }
 
-  charPrint(c = 'x') {
+  charPrint (c = 'x') {
     for (let i = 0; i < this.height; i++) {
       console.log(''.padEnd(this.width, c));
     }
