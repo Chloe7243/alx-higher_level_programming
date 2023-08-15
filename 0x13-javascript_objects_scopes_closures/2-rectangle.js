583d199d332f8070bf7ef61ff80f690f43b36e62
Stephanie <Stephaniechloe21@gmail.com>
2023-08-15 15:02:07 +0000

    Tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!w || w < 0 || h < 0 || !h) {
-      const obj = {};
-      return obj;
-    } else {
+    if (w && w >= 0 && h && h >= 0) {
       this.width = w;
       this.height = h;
     }
