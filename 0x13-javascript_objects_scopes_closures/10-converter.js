33d558eae33d940d57a56de360f0facafb7395fa
Stephanie <Stephaniechloe21@gmail.com>
2023-08-15 22:48:01 +0000

    Laziness

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  const myConverter = (n) => n.toString(base);
+  function myConverter (n) {
+    n.toString(base);
+  }
   return myConverter;
 };
