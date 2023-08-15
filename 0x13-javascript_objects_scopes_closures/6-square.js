8f4d0f466ae5ad96e2bf57b217a16a1f12176c50
Stephanie <Stephaniechloe21@gmail.com>
2023-08-15 20:22:59 +0000

    6 fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,6 @@ const MSquare = require('./5-square');
 module.exports = class Square extends MSquare {
   constructor (size) {
     super(size);
-    this.size = size;
   }
 
   charPrint (c = 'x') {
