a4f80ce10a489f6260f990db3152ce2e49ce1122
Stephanie <Stephaniechloe21@gmail.com>
2023-08-20 16:48:22 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name = {argv[4]}")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
