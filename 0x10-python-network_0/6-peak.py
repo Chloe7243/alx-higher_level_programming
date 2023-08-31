19ac79013cc676fa44fe0e877e107a2971301a0b
Stephanie <Stephaniechloe21@gmail.com>
2023-08-31 10:55:46 -0700

    6.4

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@ finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
     loi = list_of_integers
     loil = len(loi)
     if (loil == 0):
