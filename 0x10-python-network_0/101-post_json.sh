e34fe02921b04d14d52d42b4f67be0ef4d7bab3c
Stephanie <Stephaniechloe21@gmail.com>
2023-08-31 13:23:11 -0700

    101 fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of the response if status is 200
-curl -sXH "POST" "Content-Type: application/json" -d $2 $1
+curl -sXH "POST" "Content-Type: application/json" -d @$2 $1
