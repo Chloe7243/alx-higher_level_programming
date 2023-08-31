5dfaba30d3144c77085db49c5e6f5468432b4ea9
Stephanie <Stephaniechloe21@gmail.com>
2023-08-31 10:39:39 -0700

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+finds a peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    loi = list_of_integers
+    loil = len(loi)
+    if (loil == 0):
+        return None
+    else:
+        i = 0;
+        while (i + 1 < loil and loi[i + 1] > loi[i]):
+            i += 1
+        return loi[i]
