e4ff0816c42b96f86a6d34b553f966bf3d21d20c
Stephanie <Stephaniechloe21@gmail.com>
2023-08-31 10:45:49 -0700

    6.1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,14 @@ finds a peak in a list of unsorted integers.
 """
 
 
-def find_peak(list_of_integers):
-    loi = list_of_integers
-    loil = len(loi)
-    if (loil == 0):
-        return None
-    else:
-        i = 0
-        while (i + 1 < loil and loi[i + 1] > loi[i]):
-            i += 1
+if __name__ == "__main__":
+    def find_peak(list_of_integers):
+        loi = list_of_integers
+        loil = len(loi)
+        if (loil == 0):
+            return None
+        else:
+            i = 0
+            while (i + 1 < loil and loi[i + 1] > loi[i]):
+                i += 1
         return loi[i]
