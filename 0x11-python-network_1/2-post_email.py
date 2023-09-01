78c9ec05abd060bca98bed40f6b10d5f0b41a0d3
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 09:26:01 -0700

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = parse.urlencode({'email': argv[2]}).encode()
+    req = request.Request(argv[1], data=data)
+    with request.urlopen(req) as res:
+        print(str(res.read())[2:-1])
