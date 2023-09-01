b4b7eaacd13de1d5f7afc6cfc6d8d7b6a6e3fa07
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 15:18:20 -0700

    fixed?

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,12 +7,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    try:
-        url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
-        res = requests.get(url)
-        data = res.json()
-        for i in range(10):
-            res = f"{data[i]['sha']}: {data[i]['commit']['author']['name']}"
-            print(res)
-    except ValueError as e:
-        print(None)
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    res = requests.get(url)
+    data = res.json()
+    for i in range(10):
+        res = f"{data[i]['sha']}: {data[i]['commit']['author']['name']}"
+        print(res)
