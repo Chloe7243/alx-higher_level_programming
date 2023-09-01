35ef42bd7238a453ec7a4f41c61a99bcdbcaccdb
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 12:03:14 -0700

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""  a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    print(res.headers.get("X-Request-Id"))
