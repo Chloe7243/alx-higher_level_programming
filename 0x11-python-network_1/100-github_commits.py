f66b7f5d4eff974f52b8bffbc6302c507a147e3b
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 15:27:08 -0700

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     res = requests.get(url)
     data = res.json()
-    for i in range(10):
-        res = f"{data[i]['sha']}: {data[i]['commit']['author']['name']}"
+    for data in data[:10]:
+        res = f"{data['sha']}: {data['commit']['author']['name']}"
         print(res)
