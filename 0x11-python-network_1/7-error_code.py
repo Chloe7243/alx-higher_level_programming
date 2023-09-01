02846ae587f6673983a47cbafce8379b042b04e6
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 12:30:23 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        res = requests.get(argv[1])
+        status = res.raise_for_status()
+        if (status != None):
+            print(res.text)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {str(e)[:3]}")
+~                                          
