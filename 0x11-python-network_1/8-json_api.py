dfd257bde5d1e82a5e5301a573f186c1dc0fe78f
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 13:16:07 -0700

    task 8 forgot

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         else:
             print(f"{[data.get('id')]} {data.get('name')}")
     except requests.exceptions.InvalidJSONError as e:
-        print(e)
+        print("Not a valid JSON")
