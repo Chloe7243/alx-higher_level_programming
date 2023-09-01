6988ccec1af2be0b1cfc0c55c83508c9aa285e72
Stephanie <Stephaniechloe21@gmail.com>
2023-09-01 08:31:35 -0700

    task 0 and a read me

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  a Python script that fetches https://alx-intranet.hbtn.io/status """
+from urllib import request
+
+
+if __name__ == "__main__":
+    req = request.Request("https://alx-intranet.hbtn.io/status")
+    with request.urlopen(req) as res:
+        body = res.read()
+        print('Body response:')
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
